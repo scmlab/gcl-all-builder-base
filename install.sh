22fac91aac9aee491ceeadcf486cfa4d7bda7999
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-06-12 17:41:45 +0800

    Remove unnecessary sudo from apt-key command in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 apt-get update
 apt-get install -y wget software-properties-common
 
-wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-12 main" | tee /etc/apt/sources.list.d/llvm.list
 apt-get update
 apt-get install -y llvm-12 llvm-12-dev llvm-12-tools clang-12
