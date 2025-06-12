f98511084d413a36c63b2e32235c39f0681a8706
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-06-12 15:42:28 +0800

    Add missing dependencies for Haskell setup in install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,14 @@ apt-get install -y llvm-12 llvm-12-dev llvm-12-tools clang-12
 ln -sf /usr/bin/llc-12 /usr/bin/llc
 ln -sf /usr/bin/opt-12 /usr/bin/opt
 
-# ...
+
+# I don't know why I didn't need these on another machine.
+# https://github.com/haskell/ghcup-www/blob/6ea0d7169a580e237d7096289f297d53bdbcf0f1/docs/install.md?plain=1#L43
+# https://github.com/haskell/ghcup-hs/issues/421
+apt-get install -y libgmp-dev libgmp10
+
+
+# bug: https://github.com/haskell/ghcup-www/blob/6ea0d7169a580e237d7096289f297d53bdbcf0f1/docs/about.md?plain=1#L216
 apt-get install -y libnuma-dev
 
 # Clean up
