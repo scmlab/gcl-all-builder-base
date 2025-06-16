785b9dd5b93f5cc572abe0cb6622d8740e1040d6
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-06-16 18:13:24 +0800

    Refactor installation script to improve debugging output and fix npm command typo

--- install-nvm-and-node.sh
+++ install-nvm-and-node.sh
@@ -9,6 +9,15 @@ env
 
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+echo "###############000"
+cat $HOME/.bashrc | grep nvm
+echo "###############111"
 source $HOME/.bashrc
+echo "###############222"
+echo $PATH
+echo "###############333"
+which nvm
+echo "###############444"
 nvm install 18
-npm use 18
+nvm use 18
