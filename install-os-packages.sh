865ce93cf27bb5be0b8e76479351e9accde978f3
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2025-06-16 14:42:18 +0800

    remove code that adds the  llvm repository

--- install-os-packages.sh
+++ install-os-packages.sh
@@ -10,11 +10,6 @@ env
 
 # Update package list and install necessary packages
 apt-get update
-apt-get install -y wget software-properties-common
-
-wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-12 main" | tee /etc/apt/sources.list.d/llvm.list
-apt-get update
 apt-get install -y llvm-12 llvm-12-dev llvm-12-tools clang-12
 ln -sf /usr/bin/llc-12 /usr/bin/llc
 ln -sf /usr/bin/opt-12 /usr/bin/opt
@@ -23,7 +18,6 @@ ln -sf /usr/bin/opt-12 /usr/bin/opt
 # https://github.com/haskell/ghcup-www/blob/6ea0d7169a580e237d7096289f297d53bdbcf0f1/docs/install.md?plain=1#L43
 # https://github.com/haskell/ghcup-hs/issues/421
 apt-get install -y build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 pkg-config
-
 
 # bug: https://github.com/haskell/ghcup-www/blob/6ea0d7169a580e237d7096289f297d53bdbcf0f1/docs/about.md?plain=1#L216
 apt-get install -y libnuma-dev
